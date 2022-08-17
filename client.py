9254b28107f1795bbefa32e671129c677a3b92a7
SJP Math <sjpmath314@gmail.com>
2022-08-17 17:08:51 +0800

    operate videocam

--- client.py
+++ client.py
@@ -16,7 +16,19 @@ def opt():
 
 def main():
     args = opt()
-    print(args)
+
+    webcam = cv2.VideoCapture(0)
+    webcam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+    while True:
+        _, frame = webcam.read() # read image: boolean, pixel save frame
+        print(type(frame))
+        cv2.imshow('window', frame) #show image on pc
+        key = cv2.waitKey(33) # 33ms show image - speed - frame per sec
+        # change user key input to ASCII
+        if key == ord('q'): # change q to ASCII
+            break
 
 if __name__ == '__main__':
     main()
