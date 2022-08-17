2ba4b7c6c5dc7fce073ceaf7a814f401f27d57c5
SJP Math <sjpmath314@gmail.com>
2022-08-17 16:54:48 +0800

    make client.py

--- client.py
+++ client.py
@@ -1,2 +1,22 @@
+#grpc-related
+import grpc
+import proto_sample_pb2, proto_sample_pb2_grpc
+
 import os
 import cv2
+import argparse
+
+#options 10651
+def opt():
+    #object/instance
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ip', type=str, default='localhost:', help='ip address (default: localhost)')
+    parser.add_argument('--port', type=str, default='10651', help='port address (default: 10651)')
+    return parser.parse_args()
+
+def main():
+    args = opt()
+    print(args)
+
+if __name__ == '__main__':
+    main()
