4a15a48d63933c154b744fe6a2c053c66d496f99
SJP Math <sjpmath314@gmail.com>
2022-08-31 17:32:54 +0800

    added user calibration

--- client.py
+++ client.py
@@ -1,12 +1,13 @@
 #grpc-related
 import grpc
 import proto_sample_pb2, proto_sample_pb2_grpc
+from calibration import Calibration
 
 import os
 import cv2
 import argparse
 
-_THRES_DISTANCE = 95
+
 class Message:
     m_dict = {
         'absent': {
@@ -26,17 +27,17 @@ class Message:
         }
     }
 
-def userfeedback(distance, frame):
+def userfeedback(distance, frame, thresdistance):
     keyname = ''
     if distance ==0: #error
         keyname = 'absent'
         # feature for pausing video
     elif distance==-1: #server error
         keyname = 'error'
-    elif distance < _THRES_DISTANCE: #too close
+    elif distance < thresdistance: #too close
         keyname = 'too_close'
         os.system('say face is too close') # text to speech
-    frame = cv2.putText(frame, str(distance), (100,100), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,0,255), 2) # put text in image
+    frame = cv2.putText(frame, 'Detected distance: {}'.format(distance), (100,200), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,0,255), 2) # put text in image
     if keyname!='':
         frame = cv2.putText(frame, Message.m_dict[keyname]['text'], Message.m_dict[keyname]['loc'], cv2.FONT_HERSHEY_SIMPLEX, 1.0, Message.m_dict[keyname]['color'], 2)
     return frame
@@ -71,11 +72,15 @@ def main():
     webcam = cv2.VideoCapture(0)
     webcam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
     webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    cal = Calibration(webcam, grpc_request)
+    calibratedDistance = cal(args.ip, args.port)
+
 
     while True:
         _, frame = webcam.read() # read image: boolean, pixel save frame
         response = grpc_request(args.ip, args.port, frame)
-        frame = userfeedback(response.distance, frame)
+        frame = userfeedback(response.distance, frame, calibratedDistance)
+        frame = cv2.putText(frame, 'Calibrated distance: {}'.format(calibratedDistance), (100,100), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,0,255), 2) # put text in imag
         cv2.imshow('window', frame) #show image on pc
         key = cv2.waitKey(33) # 33ms show image - speed - frame per sec
         # change user key input to ASCII
